dac4152ded13202cc7995548f0adf9a10d66d020
Tota <tota.braz@ufrn.edu.br>
2020-03-06 10:27:31 -0300

    build

---
+++ build/precache-manifest.46ec3a3da8f24ccc1259ed05831875c5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e472bb14b4f458f309efcd2089eefd58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43935b5fe041c272862a",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "03869014658ee996eb97",
+    "url": "/static/css/4.5fff1b9c.chunk.css"
+  },
+  {
+    "revision": "218e533789b72d9bbc2c",
+    "url": "/static/css/main.ba52942a.chunk.css"
+  },
+  {
+    "revision": "43935b5fe041c272862a",
+    "url": "/static/js/2.7fb54b11.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/2.7fb54b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c51e7fe5ca7cdaa285",
+    "url": "/static/js/3.bb3f8d9b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/3.bb3f8d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03869014658ee996eb97",
+    "url": "/static/js/4.cc5edf52.chunk.js"
+  },
+  {
+    "revision": "218e533789b72d9bbc2c",
+    "url": "/static/js/main.96f6e808.chunk.js"
+  },
+  {
+    "revision": "06823517e3edb19e3ca3",
+    "url": "/static/js/runtime-main.45a6b539.js"
+  },
+  {
+    "revision": "49b12e2926d194c28152edc6c1db7ccb",
+    "url": "/static/media/bg_pager.49b12e29.png"
+  },
+  {
+    "revision": "bd5fdf8b8e93cb6d500d416026c601d6",
+    "url": "/static/media/logo_revista_h.bd5fdf8b.png"
+  },
+  {
+    "revision": "e531ad15e6512e9327a29f492800b568",
+    "url": "/static/media/logo_tj.e531ad15.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
